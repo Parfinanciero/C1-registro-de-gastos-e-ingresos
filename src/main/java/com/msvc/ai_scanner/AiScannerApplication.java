ca80faa9327a2ddcb5526cef85af3ef5446d31df
jerocruz76 <jerocruz76@gmail.com>
2025-01-22 11:33:04 -0500

    add work environment

---
+++ src/main/java/com/msvc/ai_scanner/AiScannerApplication.java
@@ -0,0 +1,13 @@
+package com.msvc.ai_scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msvc/ai_scanner/AiScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvc.ai_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
