a52000b3c359659989e15f660975701c65d59732
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2025-01-24 15:53:09 -0500

    add: @Field in attributes of BillEntity to change the name of some attributes

--- src/main/java/com/msvc/ai_scanner/model/entities/Bill.java
+++ src/main/java/com/msvc/ai_scanner/model/entities/Bill.java
@@ -5,6 +5,7 @@ import com.msvc.ai_scanner.model.enums.Type;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 
@@ -14,9 +15,13 @@ public class Bill {
     @Id
     private Long id;
     private String company;
+    @Field(name = "created_at")
     private LocalDateTime createdAt;
+    @Field(name = "bill_date")
+    private LocalDateTime billDate;
     private Category category;
     private Type type;
     private Long amount;
+    @Field(name = "user_id")
     private Long userId;
 }
--- src/main/java/com/msvc/ai_scanner/repositories/BillRepository.java
+++ src/main/java/com/msvc/ai_scanner/repositories/BillRepository.java
@@ -3,5 +3,8 @@ package com.msvc.ai_scanner.repositories;
 import com.msvc.ai_scanner.model.entities.Bill;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface BillRepository extends MongoRepository<Bill, Long> {
+    List<Bill> findBy
 }
