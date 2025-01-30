533c44aa3b1bc9e8e019ebcfe35354eeb9fedaed
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2025-01-29 21:26:53 -0500

    fix: annotation @NotBlank in BillDto->Type, because it should to be used only in Strings

--- src/main/java/com/msvc/ai_scanner/dto/request/BillDtoWoCreatedAt.java
+++ src/main/java/com/msvc/ai_scanner/dto/request/BillDtoWoCreatedAt.java
@@ -3,6 +3,7 @@ package com.msvc.ai_scanner.dto.request;
 import com.msvc.ai_scanner.model.enums.Category;
 import com.msvc.ai_scanner.model.enums.Type;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,7 +20,7 @@ public class BillDtoWoCreatedAt {
     private Category category;
     @NotBlank(message = "Bill date is required")
     private LocalDateTime billDate;
-    @NotBlank(message = "type is required")
+    @NotNull(message = "type is required")
     private Type type;
     @NotBlank(message = "amount is required")
     private Long amount;
